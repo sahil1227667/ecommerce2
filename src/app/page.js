bd7734c92b499f35000f9213a723ef3bf2403030
Sahil <singhsahil1227@gmail.com>
2024-09-08 03:40:07 +0530

    commit changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,172 @@
-import Image from "next/image";
+"use client";
+
+import { GlobalContext } from "@/context";
+import { getAllAdminProducts } from "@/services/product";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { isAuthUser } = useContext(GlobalContext);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [products, setProducts] = useState([]);
+  const router = useRouter();
+
+  async function getListOfProducts() {
+    const res = await getAllAdminProducts();
+
+    if (res.success) {
+      setProducts(res.data);
+    }
+  }
+
+  useEffect(() => {
+    getListOfProducts();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-500 md:text-5xl xl:text-6xl">
+              Best Accessory Collection
+            </h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+              Created by Kritika Singh & Sahil Singh
+            </p>
+
+            <button
+              type="button"
+              onClick={() => router.push("/product/listing/all-products")}
+              className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+            >
+              Explore Shop Collection
+            </button>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img
+              src="https://i.pinimg.com/564x/34/2b/19/342b19a71e0d8a41b88cc1b2c7c6ac3e.jpg"
+              alt="Explore Shop Collection"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
+            <div className="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
+              <div className="max-w-md mx-auto text-center lg:text-left">
+                <div>
+                  <h2 className="text-xl font-bold text-gray-700 sm:text-3xl">
+                    Summer Sale Collection
+                  </h2>
+                </div>
+                <button
+                  onClick={() => router.push("/product/listing/all-products")}
+                  className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                >
+                  Shop ALL
+                </button>
+              </div>
+            </div>
+            <div className="lg:col-span-2 lg:py-8">
+              <ul className="grid grid-cols-2 gap-4">
+                {products && products.length
+                  ? products
+                      .filter((item) => item.onSale === "yes")
+                      .splice(0, 2)
+                      .map((productItem) => (
+                        <li
+                          onClick={() =>
+                            router.push(`/product/${productItem._id}`)
+                          }
+                          className="cursor-pointer"
+                          key={productItem._id}
+                        >
+                          <div>
+                            <img
+                              src={productItem.imageUrl}
+                              alt="Sale Product Item"
+                              className="object-cover w-full rounded aspect-square"
+                            />
+                          </div>
+                          <div className="mt-3">
+                            <h3 className="font-medium text-gray-900">
+                              {productItem.name}
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-800">
+                              ${productItem.price}{" "}
+                              <span className="text-red-700">{`(-${productItem.priceDrop}%) Off`}</span>
+                            </p>
+                          </div>
+                        </li>
+                      ))
+                  : null}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-xl font-bold text-gray-700 sm:text-3xl">
+              SHOP BY CATEGORY
+            </h2>
+          </div>
+          <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+            <li>
+              <div className="relative block group">
+                <img
+                  src="https://www.thejewelleryeditor.com/media/images_thumbnails/filer_public_thumbnails/filer_public/f1/80/f18069ce-683d-4fca-87b2-854cef303a5d/shaun_leane_-_arc_-_couple_arc_single.jpg__760x0_q75_crop-scale_subsampling-2_upscale-false.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">UNISEX</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/kids")}
+                    className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="relative block group">
+                <img
+                  src="https://media.tiffany.com/is/image/tiffanydm/2024-TITAN-BG-2x2-3?$tile$&wid=1488&hei=1488"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">WOMEN</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/women")}
+                    className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+              <div className="relative block group">
+                <img
+                  src="https://assets.vogue.com/photos/653680eff5a1ae18efc7c36f/master/w_1920,c_limit/00-story%20(1).jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-medium text-white">MEN</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/men")}
+                    className="mt-1.5 inline-block bg-gray-700 px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
   );
 }
