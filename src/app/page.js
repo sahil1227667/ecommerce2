ca83118434feefd845dda85f092be451906220a0
Sahil <singhsahil1227@gmail.com>
2024-09-08 16:15:22 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="grid max-w-screen-xl  px-4 py-8 mx-suto  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-500 md:text-5xl xl:text-6xl">
               Best Accessory Collection
